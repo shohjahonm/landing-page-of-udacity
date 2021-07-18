3a6eb1b8cc4873f0dfd9f405eb191b53a2d43fb1
shohjahonm <85177754+shohjahonm@users.noreply.github.com>
2021-07-18 15:15:28 +0500

    Add files via upload

---
+++ landing-page/README.md
@@ -0,0 +1,23 @@
+# The General text
+This is the landing page project READme
+
+## The languages used:
+- HTML
+- CSS
+- JAVASCRIPT
+
+## The functionalities
+
+- The Navigation
+- The dynamic active
+- The scrolling effect
+
+## What did we learned:
+IN this project we converted the static web project to a dynamic project
+
+## How to use the project:
+
+First you open your browser
+Second you click 'ctrl+O'
+Third you navigate to your index.html
+forth you click on it
---
+++ landing-page/js/app.js
@@ -0,0 +1,121 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+ */
+
+/**
+ * Define Global Variables
+ * 
+ */
+// navigation global var
+const navigation = document.getElementById('navbar__list');
+// sections global var
+const sections = document.querySelectorAll('section');
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+ */
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+ */
+
+// build the nav
+
+const navBuilder = () => {
+
+    let navUI = '';
+    // looping over all sections
+    sections.forEach(section => {
+
+        const sectionID = section.id;
+        const sectionDataNav = section.dataset.nav;
+
+        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`
+
+    });
+    // append all elements to the navigation
+    navigation.innerHTML = navUI;
+
+};
+
+navBuilder();
+
+// Add class 'active' to section when near top of viewport
+
+// Getting the largest value that's less or equal to the number 
+const offset = (section) => {
+    return Math.floor(section.getBoundingClientRect().top);
+};
+
+//Remove the active class
+const removeActive = (conditional, section) => {
+    if (conditional) {
+        section.classList.add('your-active-class');
+        section.style.cssText = "linear-gradient(0deg, rgba(255, 255, 255, .1) 0%, rgba(255, 255, 255, .2) 100%);";
+    };
+
+};
+
+// Adding the active class
+const addActive = (conditional, section) => {
+    if (conditional) {
+        section.classList.add('your-active-class');
+        section.style.cssText = "linear-gradient(0deg, rgba(255, 255, 255, .1) 0%, rgba(255, 255, 255, .2) 100%);";
+    };
+};
+
+// Implementating the actual function 
+
+const sectionActivation = () => {
+    sections.forEach(section => {
+        const elementOffset = offset(section);
+
+        inviewport = () => elementOffset < 150 && elementOffset >= -150;
+        addActive(inviewport(), section);
+    });
+};
+
+window.addEventListener('scroll', sectionActivation);
+
+// Scroll to anchor ID using scrollTO event
+
+const scrolling = () => {
+
+    const links = document.querySelectorAll('navbar__menu a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            for (i = 0; i < sections; i++) {
+                sections[i].addEventListener("click", sectionScroll(link));
+            };
+        });
+    });
+};
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+ */
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
